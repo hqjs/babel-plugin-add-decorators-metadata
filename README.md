c8fc5c67dbfd3f8f3d33fbdc7ebb8c029e0fad55
Yuri <yuri.karadzhov@gmail.com>
2020-09-23 19:22:45 +0300

    Wrap angular providers decorators, allow non collable decorators

--- README.md
+++ README.md
@@ -5,3 +5,64 @@ Add decorators metadata
 ```sh
 npm install hqjs@babel-plugin-add-decorators-metadata
 ```
+
+# Usage
+```json
+{
+  "plugins": [["hqjs@babel-plugin-add-decorators-metadata"]]
+}
+```
+
+# Transformation
+Transforms decorators into class metadata, takes care of wrapping `providers` property into an object. This transformations allow angular to use dependency injection.123
+
+So the code
+```ts
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {CoreConfig} from './app.service';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    CoreConfig
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+```
+will turn into
+
+```ts
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CoreConfig } from './app.service';
+import { AppComponent } from './app.component';
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{
+    provide: CoreConfig
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+AppModule.decorators = [{
+  type: NgModule,
+  args: [{
+    declarations: [AppComponent],
+    imports: [BrowserModule],
+    providers: [{
+      provide: CoreConfig
+    }],
+    bootstrap: [AppComponent]
+  }]
+}];
+```
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const ANGULAR_PROVIDERS_DECORATORS = ['NgModule', 'Component'];
+
 module.exports = function({ types: t }) {
+  const args = t.identifier('args');
+  const decorators = t.identifier('decorators');
+  const provide = t.identifier('provide');
+  const type = t.identifier('type');
   return {
     visitor: {
       ClassDeclaration(nodePath) {
@@ -10,18 +16,34 @@ module.exports = function({ types: t }) {
             '=',
             t.memberExpression(
               t.identifier(name),
-              t.identifier('decorators')
+              decorators
             ),
-            t.arrayExpression(node.decorators.map(d => t.objectExpression([
-              t.objectProperty(
-                t.identifier('type'),
-                t.identifier(d.expression.callee.name)
-              ),
-              t.objectProperty(
-                t.identifier('args'),
-                t.arrayExpression(d.expression.arguments)
-              ),
-            ])))
+            t.arrayExpression(node.decorators.map(d => t.isCallExpression(d.expression) ?
+              t.objectExpression([
+                t.objectProperty(
+                  type,
+                  t.identifier(d.expression.callee.name)
+                ),
+                t.objectProperty(
+                  args,
+                  t.arrayExpression(d.expression.arguments.map(arg => {
+                    if (!ANGULAR_PROVIDERS_DECORATORS.includes(d.expression.callee.name)) return arg;
+                    const provider = arg.properties.find(p => p.key && p.key.name === 'providers');
+                    if (!provider) return arg;
+                    provider.value.elements = provider.value.elements.map(el => t.isObjectExpression(el) && el.properties.find(pr => pr.key.name === 'provide') ?
+                      el :
+                      t.objectExpression([
+                        t.objectProperty(provide, el)
+                      ])
+                    );
+                    return arg;
+                  }))
+                ),
+              ]) :
+              t.objectExpression([
+                t.objectProperty(type, d.expression)
+              ])
+            ))
           );
 
           nodePath.insertAfter(t.expressionStatement(decoratorsMetadata));
